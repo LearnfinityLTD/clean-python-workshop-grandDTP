c3366b136263fda2feadddb31e61051c8828c4f9
LearnfinityLTD <support@learnfinity.co.uk>
2025-10-01 23:17:55 +0100

    Update messy_analysis.py

--- code-examples/messy-example/messy_analysis.py
+++ code-examples/messy-example/messy_analysis.py
@@ -1,14 +1,29 @@
-# Messy script - show this first
-import pandas as pd
+# messy_analysis.py
+# Just some code I wrote to analyze survey data
 
-d = pd.read_csv('survey_data.csv')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+d = pd.read_csv('data.csv')
 d = d.dropna()
-d['c'] = d['score'] * 2
-m = d['c'].mean()
+d['x'] = d['col1'] * 2
+m = d['x'].mean()
 print(m)
+
+plt.figure()
+plt.plot(d['col1'], d['x'])
+plt.show()
 
 r = []
 for i in range(len(d)):
-    if d.iloc[i]['score'] > 50:
-        r.append(d.iloc[i]['c'])
+    if d.iloc[i]['col1'] > 5:
+        r.append(d.iloc[i]['x'])
+    
 print(len(r))
+
+# Some more calculations
+s = 0
+for i in range(len(r)):
+    s += r[i]
+avg = s / len(r)
+print(avg)
